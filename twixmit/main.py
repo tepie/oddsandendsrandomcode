514f8d304dd2d5aca5727a714ff52c92d93302eb
tepietrondi <terrence.a.pietrondi@gmail.com>
2011-12-05 21:43:06 -0500

    commit twitmit

---
+++ twixmit/main.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.api import users
+from google.appengine.ext.webapp import template
+from google.appengine.api import memcache
+
+import social_keys
+import model
+import os,logging
+
+from tweepy.auth import OAuthHandler
+from tweepy.auth import API
+from tweepy.error import TweepError
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        _template_values = {}
+        user = users.get_current_user()
+        
+        user_model = None
+        
+        if user:
+            logging.info("user: %s",user.nickname)
+            social_users = model.SocialKeysForUsers.all()
+            social_users.filter("user_id =",user.user_id())
+            user_model = social_users.get()
+            
+            if not user_model == None:
+                
+                if user_model.access_token_key and user_model.access_token_secret:
+                    _template_values["needs_twitter_auth"] = False
+                    
+                    auth = OAuthHandler(social_keys.TWITTER_CONSUMER_KEY, social_keys.TWITTER_CONSUMER_SECRET)
+                    
+                    #auth.set_request_token(user_model.request_token_key, user_model.request_token_secret)
+                    auth.set_access_token(user_model.access_token_key,user_model.access_token_secret)
+                    
+                    api = API(auth)
+                    api_is_working = api.test()
+                    
+                    if not api_is_working:
+                        logging.warning("api is NOT working: %s",api_is_working)
+                    
+                    try:
+                        twitter_user = memcache.get("twitter_user:%s" % user.user_id())
+                        
+                        if twitter_user == None:
+                            twitter_user = api.me()
+                            memcache.add("twitter_user:%s" % user.user_id(), twitter_user, 60)
+                        
+                        logging.info(twitter_user)
+                        _template_values["twitter_user"] = twitter_user
+                        
+                        #search_results = memcache.get("search_results:%s" % user.user_id())
+                        #if search_results == None:
+                        #    search_q = "#twixmit" # from:%s" % twitter_user.screen_name
+                        #    search_results = api.search(q=search_q,result_type="recent")  
+                        #    memcache.add("search_results:%s" % user.user_id(), search_results, 60)
+                        
+                        
+                        #_template_values["search_results"] = search_results
+                        
+                    except TweepError as err:
+                        logging.error( "TweepError error %s: %s",err.reason,err.response)
+                        user_model.access_token_key = None
+                        user_model.access_token_secret = None
+                        user_model.put()
+                        
+                        _template_values["needs_twitter_auth"] = True
+                
+                else:
+                    _template_values["needs_twitter_auth"] = True
+            
+            else:
+                _template_values["needs_twitter_auth"] = True
+                user_model = model.SocialKeysForUsers(user_id=user.user_id())
+                user_model.put()
+            
+        else:
+            _template_values["needs_twitter_auth"] = True
+            logging.warning("user is empty")
+            
+            
+            
+        redirect_url = None
+        if _template_values["needs_twitter_auth"] and not user_model == None:
+            auth = OAuthHandler(social_keys.TWITTER_CONSUMER_KEY, social_keys.TWITTER_CONSUMER_SECRET,"http://localhost:8084/callback")
+            redirect_url = auth.get_authorization_url()
+            
+            user_model.request_token_key = auth.request_token.key
+            user_model.request_token_secret = auth.request_token.secret
+            user_model.put()
+        
+            
+        _template_values["redirect_url"] = redirect_url
+        
+        _template_values["logout_url"] = users.create_logout_url("/")
+        _template_values["login_url"] = users.create_login_url("/")
+        _template_values["user"] = user
+        _template_values["user_model"] = user_model
+        
+        _path = os.path.join(os.path.dirname(__file__), 'main.html')
+        self.response.out.write(template.render(_path, _template_values))
+        
+class CallbackHandler(webapp.RequestHandler):
+    def get(self):
+        verifier = self.request.GET.get('oauth_verifier')
+        auth = OAuthHandler(social_keys.TWITTER_CONSUMER_KEY, social_keys.TWITTER_CONSUMER_SECRET)
+        user = users.get_current_user()
+        
+        if not user: 
+            logging.warning("current user is not logged in")
+            #self.redirect("/")
+        
+        social_users = model.SocialKeysForUsers.all()
+        social_users.filter("user_id =",user.user_id())
+        user_model = social_users.get()
+        
+        #logging.info(user_model.usernickname)
+        
+        if not user_model == None and user_model.request_token_key and user_model.request_token_secret:
+            auth.set_request_token(user_model.request_token_key, user_model.request_token_secret)
+            
+            auth.get_access_token(verifier)
+            
+            user_model.access_token_key = auth.access_token.key
+            user_model.access_token_secret = auth.access_token.secret
+            
+            user_model.put()
+            
+            api = API(auth)
+            api_is_working = api.test()
+                
+            self.response.out.write("twitter api is working?: %s\n" % api_is_working)
+            
+            try:
+                twitter_user = api.me()
+                
+                memcache.add("twitter_user:%s" % user.user_id(), twitter_user, 60)
+                
+                self.response.out.write("twitter user name: %s\n" % twitter_user.screen_name)
+            except TweepError as err:
+                logging.error( "TweepError error %s: %s",err.reason,err.response)
+                self.response.out.write("TweepError: %s" % err.reason)
+            
+            logging.debug("user access tokens have been set")
+            
+            self.redirect("/")
+        
+        else: 
+            logging.warning("user model is not setup correctly: %s", user_model)
+            #self.redirect("/")
+    
+    
+    
+
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler),
+                                            ('/callback', CallbackHandler)],
+                                         debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
---
+++ twixmit/model.py
@@ -0,0 +1,15 @@
+from google.appengine.ext import db
+
+class SocialKeysForUsers(db.Model):
+    user_id = db.StringProperty(required=True)
+    access_token_key = db.StringProperty()
+    access_token_secret = db.StringProperty()
+    request_token_key = db.StringProperty()
+    request_token_secret = db.StringProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
+    updated = db.DateTimeProperty(auto_now=True)
+    
+class SocialPostsForUsers(db.Model):
+    user_id = db.StringProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    text = db.StringProperty(required=True)
